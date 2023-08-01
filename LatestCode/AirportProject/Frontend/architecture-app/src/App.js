2cd424908fb75a6e7a7eb0ab4dff1e239b5a814f
iddiya <116745505+iddiya@users.noreply.github.com>
2023-07-31 21:15:06 -0400

    Update App.js

--- LatestCode/AirportProject/Frontend/architecture-app/src/App.js
+++ LatestCode/AirportProject/Frontend/architecture-app/src/App.js
@@ -1,34 +1,41 @@
-import React, { useState }  from 'react';
-
-
+// App.js
+import React, { useState } from 'react';
 import './App.css';
-import {SearchBar} from './SearchBar';
+import { SearchBar } from './SearchBar';
 import { Details } from './Details';
+import LoginPage from './LoginPage';
+
 function App() {
-  const[res,setRes]=useState();
-  const [showContent, setShowContent] = useState(false);
-  return (
-    <div className='alignbgpage'>
+    const [res, setRes] = useState();
+    const [showContent, setShowContent] = useState(false);
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-      <div >
-        <div className='alignsearch'>
-          <h1 className='align_title'>Airport Management System</h1>
-          <SearchBar setShowContent={setShowContent} setRes={setRes}/>
-          {/* Render the results or other components based on the searchQuery if needed */}
-          {/* <p>Search Query: {searchQuery}</p> */}
-        </div>
-        <Details showContent={showContent} res={res} />
-        </div>
-    </div>
-         
-        
-         
-        
-        
-        
-      
+    const handleLoginSuccess = () => {
+        setIsLoggedIn(true);
+    }
 
-  );
+    return (
+        <div className='alignbgpage'>
+            <div>
+                <div className='alignsearch'>
+                    <h1 className='align_title'>Airport Management System</h1>
+                    {isLoggedIn ? (
+                        <>
+                            <p>You are logged in.</p>
+                            <SearchBar setShowContent={setShowContent} setRes={setRes} />
+                            {/* Render the results or other components based on the searchQuery if needed */}
+                            {/* <p>Search Query: {searchQuery}</p> */}
+                            <Details showContent={showContent} res={res} />
+                        </>
+                    ) : (
+                        <>
+                            <LoginPage onLoginSuccess={handleLoginSuccess} />
+                        </>
+                    )}
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
